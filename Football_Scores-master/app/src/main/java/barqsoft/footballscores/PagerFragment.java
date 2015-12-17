50740df89cb8ef1c7f3e58dd69bb3f704301d32a
Ali Thabet <alitabet@gmail.com>
2015-12-17 15:17:17 +0300

    Move getdayname to Utility

--- Football_Scores-master/app/src/main/java/barqsoft/footballscores/PagerFragment.java
+++ Football_Scores-master/app/src/main/java/barqsoft/footballscores/PagerFragment.java
@@ -1,6 +1,5 @@
 package barqsoft.footballscores;
 
-import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.design.widget.TabLayout;
@@ -11,7 +10,6 @@ import android.support.v4.view.ViewPager;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.text.format.Time;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -87,33 +85,7 @@ public class PagerFragment extends Fragment
         @Override
         public CharSequence getPageTitle(int position)
         {
-            return getDayName(getActivity(),System.currentTimeMillis()+((position-2)*86400000));
-        }
-        public String getDayName(Context context, long dateInMillis) {
-            // If the date is today, return the localized version of "Today" instead of the actual
-            // day name.
-
-            Time t = new Time();
-            t.setToNow();
-            int julianDay = Time.getJulianDay(dateInMillis, t.gmtoff);
-            int currentJulianDay = Time.getJulianDay(System.currentTimeMillis(), t.gmtoff);
-            if (julianDay == currentJulianDay) {
-                return context.getString(R.string.today);
-            } else if ( julianDay == currentJulianDay +1 ) {
-                return context.getString(R.string.tomorrow);
-            }
-             else if ( julianDay == currentJulianDay -1)
-            {
-                return context.getString(R.string.yesterday);
-            }
-            else
-            {
-                Time time = new Time();
-                time.setToNow();
-                // Otherwise, the format is just the day of the week (e.g "Wednesday".
-                SimpleDateFormat dayFormat = new SimpleDateFormat("EEEE");
-                return dayFormat.format(dateInMillis);
-            }
+            return Utility.getDayName(getActivity(),System.currentTimeMillis()+((position-2)*86400000));
         }
     }
 }
--- Football_Scores-master/app/src/main/java/barqsoft/footballscores/Utility.java
+++ Football_Scores-master/app/src/main/java/barqsoft/footballscores/Utility.java
@@ -1,7 +1,9 @@
 package barqsoft.footballscores;
 
 import android.content.Context;
+import android.text.format.Time;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -130,6 +132,33 @@ public class Utility
         }
 
         return String.format(Locale.getDefault(), context.getString(R.string.match_score_description), homeGoals, awayGoals);
+    }
+
+    public static String getDayName(Context context, long dateInMillis) {
+        // If the date is today, return the localized version of "Today" instead of the actual
+        // day name.
+
+        Time t = new Time();
+        t.setToNow();
+        int julianDay = Time.getJulianDay(dateInMillis, t.gmtoff);
+        int currentJulianDay = Time.getJulianDay(System.currentTimeMillis(), t.gmtoff);
+        if (julianDay == currentJulianDay) {
+            return context.getString(R.string.today);
+        } else if ( julianDay == currentJulianDay +1 ) {
+            return context.getString(R.string.tomorrow);
+        }
+        else if ( julianDay == currentJulianDay -1)
+        {
+            return context.getString(R.string.yesterday);
+        }
+        else
+        {
+            Time time = new Time();
+            time.setToNow();
+            // Otherwise, the format is just the day of the week (e.g "Wednesday".
+            SimpleDateFormat dayFormat = new SimpleDateFormat("EEEE");
+            return dayFormat.format(dateInMillis);
+        }
     }
 
     /**
